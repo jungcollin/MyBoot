2d9cc87e468f3d9f6c673d4214ce8b80b36676d9
lawsn <lawsn@doo>
2017-05-06 01:57:42 +0900

    SecurityConfig @Configuration 추가,

--- cusonar/src/main/java/com/cusonar/example/config/SecurityConfig.java
+++ cusonar/src/main/java/com/cusonar/example/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.cusonar.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import com.cusonar.example.user.service.UserService;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
